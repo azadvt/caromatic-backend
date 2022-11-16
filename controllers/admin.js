08a4ab61030efebaceb98c8a858ed85bc2ddb1b5
azadvt <azad.vt@outlook.com>
2022-11-16 11:02:59 +0530

    complete user management

--- controllers/admin.js
+++ controllers/admin.js
@@ -3,7 +3,7 @@ const asyncHandler = require('express-async-handler')
 const bcrypt = require('bcrypt')
 const Admin = require('../models/adminModel')
 const Car = require('../models/carModel')
-
+const User = require('../models/userModel')
 const loginAdmin = asyncHandler(async(req,res)=>{
     const {userName,password} = req.body
   
@@ -64,7 +64,71 @@ const addCar = asyncHandler(async(req,res)=>{
 
 })
 
+const getCar =asyncHandler( async(req,res)=>{
+    const cars = await Car.find()
+    res.json(cars)
+    })
+
+
+const deleteCar = asyncHandler(async(req,res)=>{
+    const {id} = req.query
+
+    if (!req.query.id) {
+        res.status(404)
+        throw new Error('Car not found')
+    } else{
+      const data = await  Car.findById(id)
+      if(data){
+        await data.remove()
+        res.status(200).json({message:"car deleted successfully"})
+
+      }
+      else{
+        res.status(400).json({messege:"car not removed"})
+      }
+    }
+})
+
+
+//get User
+
+const getUsers = asyncHandler(async(req,res)=>{
+    const users = await User.find()
+    res.status(200).json(users)
+
+    
+})
+
+const blockUser = asyncHandler(async(req,res)=>{
+    if (!req.query.id) {
+        res.status(404)
+        throw new Error('User not found')
+    }   
+    const user = await User.findById(req.query.id)
+if (user.isBlocked) {
+    const unBlock = await User.findByIdAndUpdate(req.query.id, {
+        isBlocked: false
+    })
+    if (unBlock) {
+        res.status(200).json({ message: `${user.name}'s Account Unblocked` })
+    } else {
+        res.status(400)
+        throw new Error('Something went wrong')
+    }
+} else {
+    const block = await User.findByIdAndUpdate(req.query.id, {
+        isBlocked: true
+    })
+    if (block) {
+        res.status(200).json({ message: `${user.name}'s Account Blocked` })
+    } else {
+        res.status(400)
+        throw new Error('Something went wrong')
+    }
+}
+})
+
 
 module.exports = {
-    loginAdmin,addCar
+    loginAdmin,addCar,getCar,deleteCar,getUsers,blockUser
 }
--- routes/admin.js
+++ routes/admin.js
@@ -1,12 +1,19 @@
 const express = require('express')
 const router  = express.Router()
 const {adminProtect} = require('../middleware/authMiddleware')
-const { loginAdmin ,addCar}= require('../controllers/admin')
+const { loginAdmin ,addCar,getCar,deleteCar,getUsers,blockUser}= require('../controllers/admin')
 
 
 router.post('/login', loginAdmin)
 
 router.post('/add-car',adminProtect,addCar)
 
+router.get('/get-cars',adminProtect,getCar)
+
+router.delete('/delete-car',adminProtect,deleteCar)
+
+router.get('/get-users',adminProtect,getUsers)
+
+router.patch('/block-user',adminProtect,blockUser)
 
 module.exports = router
